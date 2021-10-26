9428e4248813572599ad2aeff4cef80b1cd842bc
josuesalas25 <39318241+josuesalas25@users.noreply.github.com>
2021-10-26 22:18:17 +0200

    primeros pasos

---
+++ 01MiPrimerPrograma.py
@@ -0,0 +1,2 @@
+# Este es mi primer programa Python
+print('Mi first program in Pyhon. I am very happy')
---
+++ 02secuenciaBasica.py
@@ -0,0 +1,20 @@
+#Realizar operacion basica
+x=2
+print(x)
+x=x+200
+print(x)
+
+#Condiciones -> Sentencia if
+x=25
+if x<10:
+    print('Pequeño')
+if x>20:
+    print('Grande')
+print('Terminado Secuencia')
+
+#Repeticiones --> Sentencia while
+n=5
+while n>0:
+    print(n)
+    n=n-1
+print('Terminado Repeticion')

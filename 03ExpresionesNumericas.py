f38f274dd68729d754c5315bd429bd663b00bfa7
josuesalas25 <39318241+josuesalas25@users.noreply.github.com>
2021-10-29 23:08:49 +0200

    nuevos programas

---
+++ 03ExpresionesNumericas.py
@@ -0,0 +1,29 @@
+# Expresiones numericas
+
+##Suma
+a=2
+a = a + 5
+print(a)
+print(a+int(99.12)) #convertir float a int  
+
+ 
+##Resta
+b = 5
+b = b - 3
+print(b)
+
+##Multiplicacion --> LA QUE SE EJECUTARA SEGUNDO
+c = 6 * 10
+print(c)
+
+##Division --> LA QUE SE EJECUTARA SEGUNDO
+d = 10/5
+print(d)
+
+##Resto
+e = 8907343%2
+print(e)
+
+##Potencia --> LA QUE SE EJECUTARA PRIMERO
+f = 10**0
+print(f)
